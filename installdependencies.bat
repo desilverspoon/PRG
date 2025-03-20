b12b501f3169c25451d6d39f29e7ef6d5f568076
desilverspoon <csilunatic@gmail.com>
2025-03-20 17:32:23 -0400

    installdependencies.bat

---
+++ installdependencies.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing required Python libraries...
+pip install selenium
+pip install matplotlib
+pip install schedule
+echo All dependencies have been installed successfully!
+pause
